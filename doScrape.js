efeff96e5a05ef8072276a12d5db85dbf5c8b813
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2021-03-15 00:42:51 -0700

    new portal init

--- doScrape.js
+++ doScrape.js
@@ -18,34 +18,69 @@ async function doScrape() {
   });
   
   // await page.waitFor(2000);
-  let elementHandle;
+  let errorElementHandle;
+  let newHandle;
   try {
-    elementHandle = await page.waitForSelector('#EndOfSurvey', { timeout: 5000 });
+    errorElementHandle = await page.waitForSelector('.openingsData.openingsNoData', { timeout: 10000 });
   } catch (err) {
-    return "Timeout on selector";
+    // return "Timeout on selector";
+    try {
+      newHandle = await page.waitForSelector('.openingsData', { timeout: 10000 });
+    }
+    catch (err) {
+      return "Timeout on selector";
+    }
   }
 
-  const paragraphsLength = await elementHandle.$$('p');
-  if (!paragraphsLength) return 'No paragraphs';
-
-  const paragraphs = await elementHandle.$$eval('p', nodes => nodes.map(n => n.innerText))
-
-  if (paragraphs.find(x => x.indexOf('no appointments') !== -1)) {
+  if (newHandle) {
     return null;
-  };
+  }
 
-  return paragraphs.join('\n');
+  // console.log(errorElementHandle);
+
+  const errorMsg = await errorElementHandle.$eval('div', el => el.innerText);
+  return errorMsg;
+  // const errorMsg = await errorElementHandle.$$('.errormessage');
+  // console.log(errorMsg.children);
+  // if (!errorMsg) return 'No error message?';
+  // else return errorMsg.innerText;
+  
+
+  // const paragraphs = await elementHandle.$$eval('p', nodes => nodes.map(n => n.innerText))
+
+  // if (paragraphs.find(x => x.indexOf('no appointments') !== -1)) {
+  //   return null;
+  // };
+
+  // return paragraphs.join('\n');
+
+  // var fullStr = "";
+  // var recurse = function (elm) {
+  //   if (elm.innerText) {
+  //     fullStr += elm.innerText;
+  //     fullStr += '\n';
+  //   }
+  //   if (elm.children) {
+  //     // return Array.from(elm.children).forEach((child) => {
+  //     //   console.log(child);
+  //     //   console.log(child.innerText);
+  //     //   recurse(child)
+  //     // })
+  //     return Array.prototype.forEach.call(elm.children, (child) => {
+  //       console.log(child.innerText);
+  //       recurse(child)
+  //     })
+  //   }
+  //   else return;
+  // }
+  // recurse(errorMsg);
+  // console.log(fullStr);
+
+  // if (fullStr.indexOf("we couldn't find any open appointments") === -1) {
+  //   return null;
+  // }
+  // else return fullStr;
 
 }
 
 module.exports = doScrape;
-
-
-// await page.pdf({
-//     path: outputFileName,
-//     displayHeaderFooter: true,
-//     headerTemplate: '',
-//     footerTemplate: '',
-//     printBackground: true,
-//     format: 'A4'
-// });

3f9db543dcd0c3686fd4bdd7330c90855943abd5
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2021-03-16 00:48:49 -0700

    support multiple recipient phone numbers, add some comments and README

--- sendMsg.js
+++ sendMsg.js
@@ -5,13 +5,17 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 const sendMsg = function (content) {
-  client.messages
-    .create({
-      body: content,
-      from: process.env.TWILIO_SMS_NUMBER,
-      to: process.env.RECIPIENT_SMS_NUMBER
-    })
-    .then(message => console.log(message.sid));
+  const recipients = process.env.RECIPIENT_SMS_NUMBERS.split(" ");
+  console.log("Sending success message to:", recipients);
+  recipients.forEach(function (recipient) {
+    client.messages
+      .create({
+        body: content,
+        from: process.env.TWILIO_SMS_NUMBER,
+        to: recipient
+      })
+      .then(message => console.log(message.sid));
+  });
 }
 
 module.exports = {
